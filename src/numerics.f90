3f9368bf8f27b2905980b7c5adbe8c2779d4ff26
Marie COMPAIN <mariecompain@MacBook-Pro-de-Marie-2.local>
2024-01-31 11:11:48 +0100

    added init.dat

--- src/numerics.f90
+++ src/numerics.f90
@@ -4,20 +4,6 @@ module numerics
     use iso_fortran_env
     IMPLICIT NONE
 
-    !--------------------------------------------!
-    !               DANS CE MODULE:
-    ! - Definitions des constantes
-    ! - Definition du type mat_creuse
-    !--------------------------------------------!
-
     integer, parameter :: rp = real64 ! double precision
     real(rp), parameter :: pi = acos(-1._rp) ! nombre pi
-
-    type mat_creuse
-        integer :: Ncoefmat ! dimension de Tmat et Jvcell
-        integer :: nb_element ! nb_element+1 = dimension de Jposi
-        real(rp), dimension(:), allocatable :: Tmat
-        integer, dimension(:), allocatable :: JvCell
-        integer, dimension(:), allocatable :: Jposi
-    end type mat_creuse
 end module numerics
